1b5e418be9adf37592221835e7dbb95a584927fe
M. QASIM <qasimygv@gmail.com>
2025-02-28 19:54:31 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+window.chtlConfig = { chatbotId: "3674989389", display: "fullscreen" };
+const script = document.createElement('script');
+script.async = true;
+script.dataset.id = "3674989389";
+script.id = "chatling-embed-script";
+script.dataset.display = "fullscreen";
+script.type = "text/javascript";
+script.src = "https://chatling.ai/js/embed.js";
+document.head.appendChild(script);
